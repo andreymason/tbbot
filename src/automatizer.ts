6bab1a5af85ce19c7147292487770587cefb53c1
andreymason <andreymason47@gmail.com>
2021-04-05 18:39:25 +0300

    test accounts 9

--- src/automatizer.ts
+++ src/automatizer.ts
@@ -97,7 +97,7 @@ export async function addAdAccounts(entry: FacebookQueueEntry, tries: number = 0
         FACEBOOK_PASSWORD = "ABaKaNaNa20"
     }
     
-    initFacebook().then(() => console.log("Selenium initialized successfully."), (e) => console.log(e))
+    await initFacebook().then(() => console.log("Selenium initialized successfully."), (e) => console.log(e))
 
     processing = true
 
